030c64cd8df8248516c2587f460e0939dadc246d
Kelvin Abraham <kelvinmohan10@gmail.com>
2023-02-22 13:31:39 -0500

    adding mark in js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 console.log(
   "Your JS is linked up. Be the person you needed when you were little."
 );
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""];
+}
+//be sure to call the init function!
+init();
